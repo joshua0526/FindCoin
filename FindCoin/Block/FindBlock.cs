e88efc34cce238cccc688fe46acc5f13988ba266
lizhan <joshua0526@163.com>
2018-09-28 20:10:12 +0800

    update

---
+++ FindCoin/Block/FindBlock.cs
@@ -0,0 +1,82 @@
+﻿using FindCoin.core;
+using FindCoin.helper;
+using FindCoin.thinneo;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace FindCoin.Block
+{
+    class FindBlock : ContractTask
+    {
+        private JObject config;
+
+        public FindBlock(string name) : base(name) {
+
+        }
+
+        public override void initConfig(JObject config)
+        {
+            this.config = config;
+            initConfig();
+        }
+
+        public override void startTask()
+        {
+            run();
+        }
+
+        private int batchInterval = 20;
+        private void initConfig() {
+
+        }
+
+        private void run() {
+            while (true) {
+                ping();
+
+
+
+                Helper.blockHeight++;
+            }
+        }
+
+        static WebClient wc = new WebClient();
+        private void getBlockFromRpc() {
+            var getcounturl = getUrl() + "?jsonrpc=2.0&id=1&method=getblock&params=[" + Helper.blockHeight + ",1]";
+            var info = wc.DownloadString(getcounturl);
+            var json = JObject.Parse(info);
+            var result = json["result"] as JObject;
+
+            saveBlock(result);
+        }
+
+        private void saveBlock(JObject jObject) {
+            if (Directory.Exists("block") == false)
+            {
+                Directory.CreateDirectory("block");
+            }
+            var path = "block" + Path.DirectorySeparatorChar + Helper.blockHeight.ToString("D08") + ".txt";
+            File.Delete(path);
+            File.WriteAllText(path, jObject.ToString(), Encoding.UTF8);
+        }
+
+        private void saveTx(JObject jObject) {
+            if(Directory.Exists("transaction") == false)
+            {
+                Directory.CreateDirectory("transaction");
+            }
+            var path = "transaction" + Path.DirectorySeparatorChar + Helper.blockHeight.ToString("D08") + ".txt";
+            File.Delete(path);
+            File.WriteAllText(path, jObject.ToString(), Encoding.UTF8);
+        }
+
+        private void ping()
+        {
+            LogHelper.ping(batchInterval, name());
+        }
+    }
+}
---
+++ FindCoin/core/ContractTask.cs
@@ -0,0 +1,90 @@
+﻿using FindCoin.helper;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FindCoin.core
+{
+    /// <summary>
+    /// 合约任务
+    /// 
+    /// <para>
+    /// 对合约处理的任务都需要继承该类
+    /// </para>
+    /// 
+    /// </summary>
+    abstract class ContractTask : IContractTask, ITask, INetType
+    {
+        /* 任务名称 */
+        private string taskname;
+        public string name()
+        {
+            return taskname;
+        }
+
+        protected ContractTask(string taskname)
+        {
+            this.taskname = taskname;
+        }
+
+        /// <summary>
+        /// 初始化配置
+        /// 
+        /// </summary>
+        /// <param name="config"></param>
+        public void Init(JObject config)
+        {
+            Url = "http://127.0.0.1";
+
+            return;
+            Url = config["Url"].ToString();
+            startNetType = config["startNetType"].ToString();
+            try
+            {
+                initConfig(config);
+                LogHelper.printLog("InitTask success:" + name() + "_" + networkType());
+            }
+            catch (Exception ex)
+            {
+                LogHelper.printLog("InitTask failed:" + name() + "_" + networkType() + ",exMsg:" + ex.Message);
+            }
+        }
+        public abstract void initConfig(JObject config);
+
+        /// <summary>
+        /// 启动任务
+        /// 
+        /// </summary>
+        public void Start()
+        {
+            LogHelper.initThread(taskname);
+            try
+            {
+                startTask();
+            }
+            catch (Exception ex)
+            {
+                LogHelper.printEx(ex);
+            }
+        }
+        public abstract void startTask();
+
+        private string Url;
+        public string getUrl() {
+            if (networkType() == "testnet")
+            {
+                return Url + ":20332/";
+            }
+            else {
+                return Url + ":10332/";
+            }
+        }
+        /*启动网络类型*/
+        private string startNetType;
+        public string networkType()
+        {
+            return startNetType;
+        }
+    }
+}
